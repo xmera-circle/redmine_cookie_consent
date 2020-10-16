54815fd63303fc63f71342ab2a50ced0660b930a
Liane Hampe <liaham@xmera.de>
2020-10-16 10:14:41 +0200

    Intial Commit

---
+++ .history/init_20201016100824.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# This file is part of the plugin Redmine Cookie Consent.
+#
+# Copyright (C) 2020 Liane Hampe <liane.hampe@xmera.de>, xmera.
+#
+# This plugin program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+Redmine::Plugin.register :redmine_cookie_consent do
+  name 'Redmine Cookie Consent plugin'
+  author 'Liane Hampe, xmera'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
