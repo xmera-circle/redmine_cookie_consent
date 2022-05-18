8e1ac7361a5857dc8626bd719bbb59923cee0f98
Liane Hampe <liaham@xmera.de>
2022-05-18 10:48:56 +0200

    Update version

--- init.rb
+++ init.rb
@@ -24,7 +24,7 @@ Redmine::Plugin.register :redmine_cookie_consent do
   name 'Redmine Cookie Consent plugin'
   author 'Liane Hampe, xmera'
   description 'Inform users about the use of cookies in Redmine'
-  version '0.0.3'
+  version '0.0.5'
 
   url 'https://circle.xmera.de/redmine-cookie-consent'
   author_url 'https://circle.xmera.de/users/5'
